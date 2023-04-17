f4d71c10f1a5cb3942d1523c94db5a41af97216e
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2023-04-17 11:58:04 -0500

    Add menu items to your order.

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 import { menuArray } from "./data.js";
 
-const orderSection = document.getElementById('order');
-
-document.addEventListener("click", function (e) {
-  // handle the clicked item based on it's id;
-  if (e.target.dataset.pizza) {
-    handlePizzaClick(e.target.dataset.pizza);
-  }
-})
-
-function handlePizzaClick(itemId) {
-  // Find the Pizza item in the menu array.
-  const targetMenuItem = menuArray.filter(function (item) {
-    return item.id === itemId;
-  })[0];
-
-  // If the order section is hidden, show it.
-  showOrder();
-
-}
-
-
-function showOrder() {
-  if (orderSection.classList.contains('hidden')) {
-    orderSection.classList.remove('hidden');
-  }
-}
+/* MENU SECTION */
 
 // Create the basic menu items.
 let menuItems = '';
@@ -48,12 +23,40 @@ menuArray.map(menuItem => {
     `
 });
 
-
 document.getElementById('content').innerHTML = menuItems;
 
-// function addItem(item) {
+/* ORDER SECTION */
 
-// } 
+const orderSection = document.getElementById('order');
+let orderItems = [];
+
+// handle the clicked item based on it's id;
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.pizza) {
+    handleAddMenuItem(e.target.dataset.pizza);
+  } else if (e.target.dataset.hamburger) {
+    handleAddMenuItem(e.target.dataset.hamburger);
+  } else if (e.target.dataset.beer) {
+    handleAddMenuItem(e.target.dataset.beer);
+  }
+})
+
+function handleAddMenuItem(itemId) {
+  const targetMenuItem = menuArray.filter(function (item) {
+    return +itemId === item.id;
+  })[0];
+
+  orderItems.push(targetMenuItem);
+  renderOrder(orderItems);
+}
+
+function showOrder() {
+  if (orderSection.classList.contains('hidden')) {
+    orderSection.classList.remove('hidden');
+  }
+}
+
+
 
 // function removeItem(item) {
 
@@ -63,21 +66,22 @@ document.getElementById('content').innerHTML = menuItems;
 
 // }
 
-// const order = `
-//     <h2 class="order-title">Your Order</h2>
-// `
-// const orderItem = `
-//     <div class="order-row">
-//         <div class="order-item">
-//             <h2>${menuItem.name}</h2>
-//             <button class="remove-item-btn">remove</button>
-//         </div>
-//         <h4>$ ${menuItem.price}</h4>
-//     </div>
-// `
+function renderOrder(orderItems) {
+  let completeOrder = ``;
+  showOrder();
 
-// document.getElementById("order").innerHTML = order;
+  console.log(orderItems);
+  orderItems.forEach(function (item) {
+    completeOrder += `
+            <div class="order-row">
+                <div class="order-item">
+                    <h2>${item.name}</h2>
+                    <button class="remove-item-btn">remove</button>
+                </div>
+                <h4>$ ${item.price}</h4>
+            </div>
+        `
+  })
 
-function renderOrder() {
-
+  document.getElementById("order").innerHTML = completeOrder;
 }
